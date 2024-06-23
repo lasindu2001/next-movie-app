f134734dba65831452e5c5b07a1f5e8f4ce48c22
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-06-23 16:06:07 +0530

    fix useSearchParams error

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,22 +1,12 @@
-'use client'
+'use client';
 
-import Link from 'next/link';
-import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
+import NavbarItemContent from './NavbarItemContent';
 
 export default function NavbarItem({ title, param }) {
-    const searchParams = useSearchParams();
-    const genre = searchParams.get('genre');
     return (
-        <div>
-            <Link
-                className={`hover:text-amber-600 font-semibold ${genre === param
-                        ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg'
-                        : ''
-                    }`}
-                href={`/?genre=${param}`}
-            >
-                {title}
-            </Link>
-        </div>
-    )
+        <Suspense fallback={<div>Loading...</div>}>
+            <NavbarItemContent title={title} param={param} />
+        </Suspense>
+    );
 }
---
+++ src/components/NavbarItemContent.jsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
+
+function NavbarItemContent({ title, param }) {
+    const searchParams = useSearchParams();
+    const genre = searchParams.get('genre');
+
+    return (
+        <div>
+            <Link
+                className={`hover:text-amber-600 font-semibold ${genre === param
+                    ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg'
+                    : ''
+                }`}
+                href={`/?genre=${param}`}
+            >
+                {title}
+            </Link>
+        </div>
+    );
+}
+
+export default NavbarItemContent;
