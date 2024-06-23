5baca74ec00afcd57bc32ba85dd0ff92d9eb9b6f
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-06-23 15:57:42 +0530

    fix error file

--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -2,7 +2,6 @@
 
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
-import React from 'react'
 
 export default function NavbarItem({ title, param }) {
     const searchParams = useSearchParams();
